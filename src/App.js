6c30ba40ef3fe414c451bbc1f3f4006e77e0da8e
Adam Timur Aslan <adamtimuraslan@gmail.com>
2022-08-12 17:59:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, {useRef, useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+ 
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isSigning, setIsSigning] = useState(false) 
+
+  useEffect(() =>{
+    const canvas = canvasRef.current;
+     canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight *2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d") 
+    context.scale(2, 2)
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 5
+    contextRef.current = context;
+    setIsSigning(true)
+  }, [] )
+
+  const startSigning = ({nativeEvent}) => {
+const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath()
+contextRef.current.moveTo(offsetX, offsetY) 
+  }
+
+  const finishedSigning = () => {
+    contextRef.current.closePath()
+setIsSigning(false)
+  }
+  const sign = (nativeEvent) => {
+    if (!isSigning){
+      return
+    } 
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+ 
+ 
+
+    <canvas 
+    onMouseDown={startSigning}
+    onMouseUp={finishedSigning}
+    onMouseMove={sign}
+    ref={canvasRef}
+    />
   );
 }
 
